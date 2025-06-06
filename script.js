dc8960a83a748eb06cdbe177897f645bba94d247
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2025-06-07 01:38:05 +0400

    bug

--- script.js
+++ script.js
@@ -9,11 +9,32 @@ const multer = require('multer');
 const cors = require("cors");
 const { upload } = require('./config/clodinary.config');
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// Enhanced CORS configuration
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'https://express-front-two.vercel.app',
+      'http://localhost:5173'
+    ];
+    
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
+// Explicitly handle OPTIONS requests
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use("/users", isAuth, userRouter);
 app.use("/posts", isAuth, postRouter);
