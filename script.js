e1f17c4f0e3e5e4f52ad9b29eb07db8c4f9c3f34
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2025-06-07 01:00:30 +0400

    bug

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const connectToDB = require("./DB/db")
-const userRouter = require("./route/user.route")
-const postRouter = require("./route/post.route")
-const authRouter = require("./auth/auth.route")
-const isAuth = require("./middlewares/isAuth.middleware")
-const multer = require('multer')
-const cors = require("cors")
+const connectToDB = require("./DB/db");
+const userRouter = require("./route/user.route");
+const postRouter = require("./route/post.route");
+const authRouter = require("./auth/auth.route");
+const isAuth = require("./middlewares/isAuth.middleware");
+const multer = require('multer');
+const cors = require("cors");
+const { upload } = require('./config/clodinary.config');
 
-const {upload} = require('./config/clodinary.config')
-
-
-
-
-app.use(cors({
-  origin: [
-    'https://express-front-two.vercel.app',
-    'http://localhost:5173'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// Enhanced CORS configuration
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'https://express-front-two.vercel.app',
+      'http://localhost:5173'
+    ];
+    
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
 
-// Handle preflight requests
-app.options('*', cors());
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use("/users",isAuth, userRouter)
-app.use("/posts", isAuth, postRouter)
-app.use("/auth", authRouter)
+// Explicitly handle OPTIONS requests
+app.options('*', cors(corsOptions));
 
+app.use(express.json());
+app.use("/users", isAuth, userRouter);
+app.use("/posts", isAuth, postRouter);
+app.use("/auth", authRouter);
 
-connectToDB()
+connectToDB();
+
 app.post('/upload', upload.single('image'), (req, res) => {
-    res.send(req.file)
-})
+    res.send(req.file);
+});
 
-app.get("/", (req, res)=>{
-    res.send("hello world")
-})
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
 
-app.listen(3000, ()=>{
-    console.log("Server is running on http://localhost:3000")
-})
-
+module.exports = app; // Important for Vercel
